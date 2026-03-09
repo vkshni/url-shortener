e672791bb9998c91fac295430afc7abf91776631
vkshni <vksdrive24@gmail.com>
2026-03-09 20:59:45 +0530

    short_code generator

---
+++ short_code_gen.py
@@ -0,0 +1,15 @@
+# Short Code Generator
+
+import string
+import random
+
+ALPHABET = string.ascii_letters + string.digits
+CODE_LENGTH = 6
+
+
+def generate() -> str:
+    pass
+
+
+def is_collision() -> bool:
+    pass
