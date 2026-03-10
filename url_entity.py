ed1774e9d84e1b0d0532d2b2ec7736569f952e9a
vkshni <vksdrive24@gmail.com>
2026-03-10 21:35:42 +0530

    entity layer added

---
+++ url_entity.py
@@ -0,0 +1,52 @@
+from uuid import uuid4
+from datetime import datetime
+
+DATE_FORMAT = "%d-%m-%YT%H:%M:%S"
+
+# URL Entity
+
+
+class URL:
+
+    def __init__(
+        self,
+        long_url: str,
+        short_code: str,
+        created_at: str = None,
+        visit_count: int = None,
+        url_id: str = None,
+    ):
+        self.url_id = str(url_id) if url_id else str(uuid4())
+        self.long_url = long_url
+        self.short_code = short_code
+        self.created_at = (
+            datetime.strptime(created_at, DATE_FORMAT) if created_at else datetime.now()
+        )
+
+        self.visit_count = visit_count if visit_count else 0
+
+    def to_dict(self):
+
+        return {
+            "url_id": self.url_id,
+            "long_url": self.long_url,
+            "short_code": self.short_code,
+            "created_at": (
+                self.created_at.strftime(DATE_FORMAT) if self.created_at else None
+            ),
+            "visit_count": self.visit_count,
+        }
+
+    @classmethod
+    def from_dict(cls, url_dict):
+
+        return cls(
+            long_url=url_dict["long_url"],
+            short_code=url_dict["short_code"],
+            created_at=url_dict["created_at"],
+            visit_count=url_dict["visit_count"],
+            url_id=url_dict["url_id"],
+        )
+
+    def validate(self):
+        raise NotImplementedError
