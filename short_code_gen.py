b1ec6ae4d471ac2694e837dd32c3a0a8606bbc0a
vkshni <vksdrive24@gmail.com>
2026-03-11 21:04:00 +0530

    methods

--- short_code_gen.py
+++ short_code_gen.py
@@ -6,10 +6,20 @@ import random
 ALPHABET = string.ascii_letters + string.digits
 CODE_LENGTH = 6
 
+def generate(code_length:int = CODE_LENGTH) -> str:
+    short_code = "".join(random.choices(ALPHABET,k=code_length))
+    return short_code
 
-def generate() -> str:
-    pass
+def is_valid_code(code:str) -> bool:
 
+    if not code or len(code) != 6:
+        return False
+    
+    return all(c in ALPHABET for c in code)
 
-def is_collision() -> bool:
-    pass
+def generate_batch(count: int=10) -> list[str]:
+
+    codes = set()
+    while len(codes) < count:
+        codes.add(generate())
+    return list(codes)
