c8ae86e01aa5aa252152a5ac69f1946c2e2b04e2
vkshni <vksdrive24@gmail.com>
2026-03-12 21:21:29 +0530

    shorten command

---
+++ main.py
@@ -0,0 +1,48 @@
+# CLI Interface
+
+import argparse
+import sys
+
+from url_service import URLService
+
+service = URLService()
+
+def cmd_shorten(args):
+
+    existing = service.db.find_by_url(args.long_url)
+    if existing:
+        print(f"✓ Already shortened: short.ly/{existing.short_code}")
+    else:
+        short_code = service.shorten(args.long_url)
+        print(f"✓ Created: short.ly/{short_code}")
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        prog= "main",
+        description="URL Shortener CLI",
+        add_help=True
+    )
+
+    sub = parser.add_subparsers(dest="command")
+    sub.required = True
+
+    #shorten
+    p_shorten = sub.add_parser("shorten", help="Shorten a long url", description="Create a short code for a long URL")
+    p_shorten.add_argument("long_url", help="URL to shorten (must start with http:// or https://)")
+    p_shorten.set_defaults(func=cmd_shorten)
+
+    args = parser.parse_args()
+
+    try:
+        args.func(args)
+    except ValueError as e:
+        print(f"❌ {e}")
+        sys.exit(1)
+    except Exception as e:
+        print(f"💥 System Error: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
